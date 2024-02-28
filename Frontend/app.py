60848ea6da73d26843787bfd0522f7d56e497c22
Adarsh Ghimire <adarshghimire10@gmail.com>
2024-02-28 14:55:54 +0545

    Length Selection Implemented

--- Frontend/app.py
+++ Frontend/app.py
@@ -6,56 +6,26 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 app = Flask(__name__)
 CORS(app)
 
-tokenizer = AutoTokenizer.from_pretrained("Kishan11/test")
-model = AutoModelForSeq2SeqLM.from_pretrained("Kishan11/test")
+#mt5 model
+tokenizer = AutoTokenizer.from_pretrained("Adarsh203/new_mT5_Sum")
+model = AutoModelForSeq2SeqLM.from_pretrained("Adarsh203/new_mT5_Sum")
 
-# from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+#mbart model
+tokenizer2 = AutoTokenizer.from_pretrained("Kishan11/mBART_SUM")
+model2 = AutoModelForSeq2SeqLM.from_pretrained("Kishan11/mBART_SUM")
 
-tokenizer2 = AutoTokenizer.from_pretrained("Adarsh203/mBART_SUM")
-model2 = AutoModelForSeq2SeqLM.from_pretrained("Adarsh203/mBART_SUM")
 
-# def split_text(text,chunk_size=200):
-#     sentences = text.split('|')
-#     chunks = []
-#     current_chunk = []
 
-#     def word_count(sentences_list):
-#         return sum(len(sentence.split()) for sentence in sentences_list)
+def format_paragraph(text):
+    # Split the text into lines, remove leading and trailing spaces from each line,
+    # and filter out any empty lines.
+    lines = [line.strip() for line in text.splitlines() if line.strip()]
     
-#     for sentence in sentences:
-#         if word_count(current_chunk+[sentence]) <= chunk_size:
-#             current_chunk.append(sentence)
-
-#         else:
-#             if current_chunk:
-#                 chunks.append('|'.join(current_chunk))
-#                 current_chunk = [sentence]
-
-#             else:
-#                 chunks.append(sentence)
+    # Join the lines with a space to ensure words are not concatenated without spacing.
+    formatted_text = ' '.join(lines)
     
-#     if current_chunk:
-#         chunks.append('|'.join(current_chunk))
+    return formatted_text
 
-#     return chunks
-
-# def summary_nepali(text,model,tokenizer,max_summary_length=512):
-#     chunks = split_text(text)
-
-#     summaries = [summary(chunk,model,tokenizer,max_summary_length) for chunk in chunks]
-
-#     final_summary = '|'.join(summaries)
-
-#     return final_summary
-
-
-
-# def summary(text, model, tokenizer, max_summary_length):
-#     inputs = tokenizer(text, return_tensors="pt", max_length=1024, truncation=True)
-#     input_ids = inputs.input_ids.to(model.device)
-#     summary_ids = model.generate(input_ids, max_length=max_summary_length, num_beams=4, length_penalty=0.1, early_stopping=True)
-#     generated_summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
-#     return generated_summary
 
 
 def split_text_by_sentence_end_in_range(text, min_chunk_size, max_chunk_size, delimiter='।'):
@@ -105,8 +75,9 @@ def summary(text, model, tokenizer, max_summary_length):
     generated_summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
     return generated_summary
 
+#long summary
 def summary_nepali(text,model,tokenizer,max_summary_length=512):
-    chunks = split_text_by_sentence_end_in_range(text,50,125)
+    chunks = split_text_by_sentence_end_in_range(text,100,150)
     for i in range(len(chunks)):
         print(chunks[i])
 
@@ -117,7 +88,21 @@ def summary_nepali(text,model,tokenizer,max_summary_length=512):
     return final_summary
 
 
+# short summary
+def mt5Summary(text,model,tokenizer,max_summary_length=512):
+    input = tokenizer(text,return_tensors="pt",max_length=max_summary_length,truncation=True)
+    summary_ids = model.generate(input.input_ids.to(model.device), 
+                                  max_length=max_summary_length, 
+                                  num_beams=5,
+                                  length_penalty=0.1,
+                                  early_stopping=True)
 
+    # Decode the generated summary
+    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+
+    return summary
+
+#short summary
 def mbartSummary(text,model,tokenizer,max_summary_length=512):
     input = tokenizer2(text,return_tensors="pt",max_length=max_summary_length,truncation=True)
     summary_ids = model.generate(input.input_ids.to(model.device), 
@@ -131,55 +116,34 @@ def mbartSummary(text,model,tokenizer,max_summary_length=512):
 
     return summary
 
-
-
-def format_paragraph(text):
-    sentences = text.split('\n\n')
-    formatted_text = '\n\n'.join(' '.join(sentence.split()) for sentence in sentences)
-    return formatted_text
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-# @app.route('/summarize', methods=['POST'])
-# def summarize():
-#     data = request.json
-#     # text = request.form['text']
-#     text = data['text']
-#     summarized_text = summary(text, model, tokenizer)
-#     # return render_template('summary.html', original_text=text, summarized_text=summarized_text)
-#     return jsonify({'summarized_text':summarized_text})
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
 @app.route('/', methods=['GET', 'POST'])
 def summarize():
-    # if request.method == 'POST':
-    #     data = request.json
-    #     text = data['text']
-    #     summarized_text = summary_nepali(text, model, tokenizer)
-    #     return jsonify({'summarized_text': summarized_text})
+  
 
     request.method == 'POST'
     data = request.json
+    # print("Received data",data)
     text = data['text']
     selected_model = data['selectedModel']
+    selected_length = data['selectedLength']
+    formatted_text = format_paragraph(text)
+    # print('Formatted text:',formatted_text)
 
-    if selected_model == 'default':
-        summarized_text = summary_nepali(text, model, tokenizer)
-    elif selected_model == 'model2':
-        summarized_text = mbartSummary(text,model2,tokenizer2)
+    if selected_model == 'model1' and selected_length== 'short':
+        summarized_text = mt5Summary(formatted_text, model, tokenizer)
+    elif selected_model == 'model1' and selected_length == 'long':
+        summarized_text = summary_nepali(formatted_text,model,tokenizer)
+    elif selected_model == 'model2' and selected_length == 'short':
+        summarized_text = mbartSummary(formatted_text,model2,tokenizer2)
+    elif selected_model == 'model2' and selected_length == 'long':
+        summarized_text = summary_nepali(formatted_text,model2,tokenizer2)
     
     try:
-        summary = jsonify({'summarized_text': summarized_text})
+        summary = jsonify({'summarized_text': summarized_text,'formatted_text':formatted_text})
     except:
         summary = jsonify({'summarized_text':'Loading..'})
     return summary
-    # else:
-    #     return render_template('index.html')
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Frontend/client/src/App.js
+++ Frontend/client/src/App.js
@@ -1,31 +1,4 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -33,19 +6,12 @@ const App = () => {
   const [text, setText] = useState('');
   const [summarizedText, setSummarizedText] = useState('');
   const [selectedModel, setSelectedModel] = useState('model1')
+  const [selectedLength, setSelectedLength] = useState('short')
 
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   const response = await fetch('/',{
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify({text,selectedModel}),
-  //   });
-  //   const data = await response.json()
-  //   setSummarizedText(data.summarized_text);
-  // };
+// counts the word
+  const countWords = (str='') => {
+    return str.split(/\s+/).filter(Boolean).length;
+  };
 
 
   const handleSubmit = async (e) => {
@@ -55,205 +21,87 @@ const App = () => {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({text,selectedModel}),
+      body: JSON.stringify({text,selectedModel,selectedLength}),
     });
-    const data = await response.json()
+    const data = await response.json();
+    console.log('Response data:',data);
+    setText(data.formatted_text);
     setSummarizedText(data.summarized_text);
   };
   
-  // return (
-  //   <div >
-  //     <h1 className='justify-center'>Text Summarizer</h1>
-  //     <form onSubmit={handleSubmit}>
-  //       <textarea value={text} onChange={(e) => setText(e.target.value)} />
-  //       <button type="submit">Summarize</button>
-  //     </form>
-  //     {summarizedText && (
-  //       <div>
-  //         <h2>Summarized Text:</h2>
-  //         <p>{summarizedText}</p>
-  //       </div>
-  //     )}
-  //   </div>
-  // )
-
-  // return (
-  //   <div className="bg-gray-100 min-h-screen flex flex-col items-center justify-center py-8">
-  //     <h1 className="text-3xl font-bold mb-8">Text Summarizer</h1>
-  //     <form onSubmit={handleSubmit} className="w-full max-w-md px-4">
-  //       <textarea
-  //         className="w-full border border-black-300 rounded p-2 mb-4"
-  //         rows="6"
-  //         value={text}
-  //         onChange={(e) => setText(e.target.value)}
-  //         placeholder="Enter text to summarize..."
-  //       />
-  //       <button
-  //         type="submit"
-  //         className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-  //       >
-  //         Summarize
-  //       </button>
-  //     </form>
-  //     {summarizedText && (
-  //       <div className="mt-8">
-  //         <h2 className="text-xl font-semibold mb-2">Summarized Text:</h2>
-  //         <p className="bg-white border border-gray-300 rounded p-4">{summarizedText}</p>
-  //       </div>
-  //     )}
-  //   </div>
-  // )
-
-  // return (
-  //   <div className="bg-gray-100 min-h-screen flex flex-col items-center justify-center py-8">
-  //     <h1 className="text-3xl font-bold mb-8">Text Summarizer</h1>
-  //     <form onSubmit={handleSubmit} className="w-full max-w-md px-4">
-  //       <textarea
-  //         className="w-full border border-gray-300 rounded-md p-2 mb-4 h-40 resize-none focus:border-blue-500"
-  //         value={text}
-  //         onChange={(e) => setText(e.target.value)}
-  //         placeholder="Enter text to summarize..."
-  //       />
-  //       <button
-  //         type="submit"
-  //         className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-  //       >
-  //         Summarize
-  //       </button>
-  //     </form>
-  //     {summarizedText && (
-  //       <div className="mt-8">
-  //         <h2 className="text-xl font-semibold mb-2">Summarized Text:</h2>
-  //         <p className="bg-white border border-gray-300 rounded p-4">{summarizedText}</p>
-  //       </div>
-  //     )}
-  //   </div>
-  // );
-//   return (
-//     <div className="container">
-//       <h1 className="heading">Text Summarizer</h1>
-//       <form onSubmit={handleSubmit}>
-//         <textarea
-//           className="textarea"
-//           value={text}
-//           onChange={(e) => setText(e.target.value)}
-//           placeholder="Enter text to summarize..."
-//         />
-//         <button type="submit" className="button">
-//           Summarize
-//         </button>
-//       </form>
-//       {summarizedText && (
-//         <div>
-//           <h2>Summarized Text:</h2>
-//           <p>{summarizedText}</p>
-//         </div>
-//       )}
-//     </div>
-//   );
-
-// }
-
-// // export default App
-
-// return (
-//   <div className="container">
-//     <div className="row">
-//       <div className="col text-center mb-4">
-//         <h1 className="display-4">Nepali News Summarizer</h1>
-//       </div>
-//     </div>
-//     <div className="row">
-//       <div className="col">
-//         <form onSubmit={handleSubmit}>
-//           <div className="mb-3">
-//             <textarea
-//               className="form-control"
-//               value={text}
-//               onChange={(e) => setText(e.target.value)}
-//               placeholder="Enter text to summarize..."
-//               rows="5" // Adjust the number of rows as needed
-//             />
-//           </div>
-//           <button type="submit" className="btn btn-primary">
-//             Summarize
-//           </button>
-//         </form>
-//       </div>
-//     </div>
-//     {summarizedText && (
-//       <div className="row mt-4">
-//         <div className="col">
-//           <h2>Summarized Text:</h2>
-//           <p className="lead">{summarizedText}</p>
-//         </div>
-//       </div>
-//     )}
-//   </div>
-// );
 
 
 return (
   <>
-  
-  <div className="container">
-    <div className="row">
-      <div className="col text-center mb-4">
-        <h1 className="display-4">Nepali News Summarizer</h1>
-      </div>
-    </div>
-    <div className="row">
-      <div className="col-md-3]">
-        <div className="mb-3">
-          <label htmlFor="modelSelect" className="form-label">Select Model</label>
-          <select 
-            id="modelSelect" 
-            className="form-select" 
-            defaultValue="default"
-            // disabled // Disable the dropdown menu
-            onChange={(e)=> setSelectedModel(e.target.value)}
-            style={{width:'150px'}}
-          >
-            <option value="default">mT5</option>
-            <option value="model2">MBART</option>
-            
-            {/* Add more options for different models */}
-          </select>
+  <div className="col text-center mb-5 bg-success">
+    <h1 className="display-4" style={{color:'white'}}>Nepali News Summarizer</h1>
+  </div>
+    <div className="container">
+      <div className="row mb-3 align-items-center">
+      
+        <div className="col-md-5 d-flex justify-content-start">
+          <p className="mb-0">Input Text Word Count: {countWords(text)}</p>
         </div>
-      </div>
-      </div>
-      <div className='row'>
-      <h2>News Article:</h2>
-      <div className="col">
-        <form onSubmit={handleSubmit}>
-          <div className="mb-3">
-            <textarea
-              className="form-control"
-              placeholder="Enter text to summarize..."
-              rows="5"
-              value={text}
-              onChange={(e) => setText(e.target.value)}
-
-            />
+            <div className="col d-flex justify-content-end">
+            <div style={{marginRight: 10}}>
+              <label htmlFor="modelSelect" className="form-label">Select Model</label>
+              <select 
+                id="modelSelect" 
+                className="form-select" 
+                value={selectedModel}
+                onChange={(e) => setSelectedModel(e.target.value)}
+              >
+                <option value="model1">mT5</option>
+                <option value="model2">MBART</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="LengthSelect" className="form-label">Select Length</label>
+              <select 
+                id="lengthSelect" 
+                className="form-select" 
+                value={selectedLength}
+                onChange={(e) => setSelectedLength(e.target.value)}
+              >
+                <option value="short">Short </option>
+                <option value="long">Long</option>
+              </select>
+            </div>
           </div>
-          <button type="submit" className="btn btn-primary"> {/* Disable the button */}
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            placeholder="Enter News Article to summarize..."
+            rows="8"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+        <div className="d-flex justify-content-between">
+          <button type="submit" className="btn btn-success">
             Summarize
           </button>
-        </form>
-      </div>
-      </div>
-    {summarizedText && (  
-    <div className="row mt-4">
-      <div className="col">
-        <h2>Summarized Text:</h2>
-        <p className="lead">{summarizedText}</p>
-      </div>
+         
+        </div>
+      </form>
+      {summarizedText && (
+        <div className="row mt-4">
+          <div className="col">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Summarized Text:</h5>
+                <p className="card-text">{summarizedText}</p>
+              </div>
+            </div>
+          </div>
+            <p className="mt-3">Summary Word Count: {countWords(summarizedText)}</p>
+        </div>
+      )}
     </div>
-    )
-}
-  </div>
   </>
 );
-    }
+};
 
 export default App
